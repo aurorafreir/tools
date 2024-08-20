e351f9133ad019791170178bce2432e0a9ac6d96
aurorapuppy <catanddogsoup@gmail.com>
2024-08-20 17:00:56 +0100

    Adding shebang and removed semicolons causing issues

--- maya/2023/launcher/maya_runner.sh
+++ maya/2023/launcher/maya_runner.sh
@@ -1,12 +1,13 @@
+#!/usr/bin/env bash
 
-TOOLS_DIR=$(dirname "$PWD"); # Gets this script's running directory"
+TOOLS_DIR=$(dirname "$PWD") # Gets this script's running directory"
 
-export PYTHONPATH="$TOOLS_DIR/python";
-export MAYA_MODULE_PATH="$TOOLS_DIR/third_party/mgear_4.2.2/release";
-#export MAYA_PLUG_IN_PATH="$SCRIPT_DIR/plugins";
-#export XBMLANGPATH="$SCRIPT_DIR/icons/%B";
-export MAYA_SHELF_PATH="$TOOLS_DIR/shelves";
+export PYTHONPATH="$TOOLS_DIR/python"
+export MAYA_MODULE_PATH="$TOOLS_DIR/third_party/mgear_4.2.2/release"
+#export MAYA_PLUG_IN_PATH="$SCRIPT_DIR/plugins"
+#export XBMLANGPATH="$SCRIPT_DIR/icons/%B"
+export MAYA_SHELF_PATH="$TOOLS_DIR/shelves"
 
-#export MGEAR_SHIFTER_COMPONENT_PATH="$SCRIPT_DIR/mGearScripts/Components/";
+#export MGEAR_SHIFTER_COMPONENT_PATH="$SCRIPT_DIR/mGearScripts/Components/"
 
-maya;
+maya
