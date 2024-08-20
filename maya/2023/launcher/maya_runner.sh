272d1671c571797c540a4a99c44800b7cdf7d8fe
aurorapuppy <catanddogsoup@gmail.com>
2024-08-20 20:26:34 +0100

    Switching $PWD for $0, and uncommenting paths

--- maya/2023/launcher/maya_runner.sh
+++ maya/2023/launcher/maya_runner.sh
@@ -1,13 +1,9 @@
-#!/usr/bin/env bash
-
-TOOLS_DIR=$(dirname "$PWD") # Gets this script's running directory"
+TOOLS_DIR=$(dirname "$0") # Gets this script's running directory"
 
 export PYTHONPATH="$TOOLS_DIR/python"
 export MAYA_MODULE_PATH="$TOOLS_DIR/third_party/mgear_4.2.2/release"
-#export MAYA_PLUG_IN_PATH="$SCRIPT_DIR/plugins"
-#export XBMLANGPATH="$SCRIPT_DIR/icons/%B"
+export MAYA_PLUG_IN_PATH="$TOOLS_DIR/plugins"
+export XBMLANGPATH="$TOOLS_DIR/icons"
 export MAYA_SHELF_PATH="$TOOLS_DIR/shelves"
-
-#export MGEAR_SHIFTER_COMPONENT_PATH="$SCRIPT_DIR/mGearScripts/Components/"
 
 maya
