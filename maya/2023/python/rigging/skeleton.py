6bafe100585e6ba4adb64453fa6d978d02f629fd
aurorapuppy <aurorafreir@gmail.com>
2025-06-14 19:05:29 +0100

    Removing unnecessary TODO

--- maya/2023/python/rigging/skeleton.py
+++ maya/2023/python/rigging/skeleton.py
@@ -9,8 +9,31 @@ import pymel.core as pm
 
 # LOCAL APPLICATION IMPORTS
 
-# TODO AFOX : Refactor, switch to PyMel
 
+def get_top_skeleton_joint():
+    world_objects = pm.ls(assemblies=True)
+    scene_skinned_joints = []
+    root = None
+    for sc in pm.ls(type="skinCluster"):
+        skinned_joints = [x for x in pm.skinCluster(sc, influence=True, query=True)]
+        new_skinned_joints = [i for i in skinned_joints if i not in scene_skinned_joints]
+        scene_skinned_joints.extend(new_skinned_joints)
+
+    # Fallback for if this is run in a guides scene without skinned joints
+    if not scene_skinned_joints:
+        scene_skinned_joints = [i for i in pm.listRelatives("rig", children=True, allDescendents=True, type="joint") if
+                                i.visibility.get()]
+
+    print(scene_skinned_joints)
+
+    for jnt in scene_skinned_joints:
+        print(jnt)
+        print(jnt, jnt.getParent())
+        if isinstance(jnt.getParent(), pm.nt.Joint):
+            continue
+        return root
+
+    return None
 
 def extract_skeleton() -> None:
     """
@@ -19,25 +42,9 @@ def extract_skeleton() -> None:
         to do so I'm leaving it for now lol
 
     """
-    world_objects = pm.ls(assemblies=True)
-    scene_skinned_joints = []
-    for sc in pm.ls(type="skinCluster"):
-        skinned_joints = [x for x in pm.skinCluster(sc, influence=True, query=True)]
-        new_skinned_joints = [i for i in skinned_joints if i not in scene_skinned_joints]
-        scene_skinned_joints.extend(new_skinned_joints)
-
-    # Fallback for if this is run in a guides scene without skinned joints
-    if not scene_skinned_joints:
-        scene_skinned_joints = [i for i in pm.listRelatives("rig", children=True, allDescendents=True, type="joint") if i.visibility.get()]
-
-    for jnt in scene_skinned_joints:
-        parent_is_joint = True if type(jnt.getParent()) == pm.nt.Joint else False
-        incoming_attrs = jnt.listConnections(plugs=True, source=True)
-        for attr in incoming_attrs:
-            pm.disconnectAttr(attr)
-        if not parent_is_joint:
-            print(f"moving {jnt} to top level")
-            pm.parent(jnt, world=True)
+    root = get_top_skeleton_joint()
+    print(f"moving {root} to top level")
+    pm.parent(root, world=True)
 
     return None
 
