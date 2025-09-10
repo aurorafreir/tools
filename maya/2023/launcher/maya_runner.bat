970850c22cadcd29ee30d783fec400e781a43575
aurorafreir <aurorafreir@gmail.com>
2025-09-10 21:33:08 +0100

    Fixing version call and updating PYTHONPATH to include /third_party/

--- maya/2023/launcher/maya_runner.bat
+++ maya/2023/launcher/maya_runner.bat
@@ -2,17 +2,27 @@
 
 :: Gets this script's running directory
 set CORE_DIR=%~dp0\..
+
+CALL :NORMALIZEPATH %CORE_DIR%
+SET CORE_DIR=%RETVAL%
+
 set SCRIPT_DIR=%CORE_DIR%/python/
 
-set PYTHONPATH=%SCRIPT_DIR%;
-:: set MAYA_MODULE_PATH=%SCRIPT_DIR%mgear_4.0.9/release;
-set XBMLANGPATH=%CORE_DIR%icons/%B
-set MAYA_SHELF_PATH=%CORE_DIR%shelves
+:: set PYTHONPATH=%SCRIPT_DIR%;
+:: set XBMLANGPATH=%CORE_DIR%icons/%B
+:: set MAYA_SHELF_PATH=%CORE_DIR%shelves
 
-set MGEAR_SHIFTER_COMPONENT_PATH=%SCRIPT_DIR%/mGearScripts/Components
+set PYTHONPATH=%SCRIPT_DIR%;%CORE_DIR%/third_party/;
+set MAYA_MODULE_PATH=%CORE_DIR%/third_party/mgear_4.2.2/release;C:\Program Files\Epic Games\MetaHumanForMaya;
+set MAYA_PLUG_IN_PATH=%CORE_DIR%/plugins
+set XBMLANGPATH=%CORE_DIR%/icons
+set MAYA_SHELF_PATH=%CORE_DIR%/shelves
+
+set MGEAR_SHIFTER_COMPONENT_PATH=%CORE_DIR%/third_party/mGearScripts/Components
+
 
 :: Actually open maya
-set KEY_NAME="HKEY_LOCAL_MACHINE\SOFTWARE\Autodesk\Maya\2022\Setup\InstallPath"
+set KEY_NAME="HKEY_LOCAL_MACHINE\SOFTWARE\Autodesk\Maya\2023\Setup\InstallPath"
 set VALUE_NAME=MAYA_INSTALL_LOCATION
 
 FOR /F "usebackq tokens=2*" %%A IN (`REG QUERY %KEY_NAME% /v %VALUE_NAME% 2^>nul`) DO (
@@ -27,3 +37,7 @@ start maya.exe
 
 :: Close the terminal window after opening Maya
 exit
+
+:NORMALIZEPATH
+  SET RETVAL=%~f1
+  EXIT /B
